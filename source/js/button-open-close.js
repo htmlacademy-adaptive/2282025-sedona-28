4586650761e9d72dd6ed0540548677acb0a83f8b
Anastasya <gluhova_an@bk.ru>
2023-05-09 20:03:35 +0400

    Добавляет svg-спрайт

--- source/js/button-open-close.js
+++ source/js/button-open-close.js
@@ -6,9 +6,9 @@ navMain.classList.remove('main-navigation--nojs');
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-navigation--closed')) {
     navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--opened');
+    navMain.classList.add('main-navigation--open');
   } else {
     navMain.classList.add('main-navigation--closed');
-    navMain.classList.remove('main-navigation--opened');
+    navMain.classList.remove('main-navigation--open');
   }
 });
